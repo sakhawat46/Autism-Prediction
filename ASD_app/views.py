c2b1d8bbd33a6683a233016ab0b0047d159fdb3d
sakhawat46 <hsakhawat135@gmail.com>
2024-01-31 18:06:06 +0600

    home page add

--- ASD_app/views.py
+++ ASD_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import keras
+from PIL import Image
+import numpy as np
+import os
+from django.core.files.storage import FileSystemStorage
+
+media = 'media'
+model = keras.models.load_model('')
 
 # Create your views here.
 
 def home(request):
     return render(request, 'ASD_app/home.html', context={})
+
+
+
+def makepredictions(path):
+    img = Image.open(path)
+    img_d = img.resize((224,224))
